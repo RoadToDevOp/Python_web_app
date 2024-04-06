df6492ae7727a924d4ac339c485459738d143ba8
Eoghan O'Neill <eoghanoneill@live.co.uk>
2024-04-06 12:01:05 +0100

    Fixes to naming

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 terraform {
-    required_providers {
-        azurerm ={
-            source = "Hashicorp/azurerm"
-            version = "~>3.0"
-        }
-        random = {
-            source = "Hashicorp/random"
-            version = "~>3.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "Hashicorp/azurerm"
+      version = "~>3.0"
     }
+    random = {
+      source  = "Hashicorp/random"
+      version = "~>3.0"
+    }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = var.resource_group_location
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 
@@ -30,7 +30,7 @@ resource "azurerm_resource_group" "rg" {
 #     storage_os_disk {
 #       name =
 #       create_option =
-      
+
 #     }
 # }
 
--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "resource_group_name" {
-    default = "Configure_resource_name"
+  default = "Configure_resource_name"
 }
 
 variable "resource_group_location" {
-    default = "uksouth"
+  default = "uksouth"
 }
 
 variable "web_vm_name" {
-    default = "default_web_vm_name"
+  default = "default_web_vm_name"
 }
 
 variable "app_vm_name" {
-    default = "default_app_vm_name"
+  default = "default_app_vm_name"
 }
 
-variable vm_name {
-  default     = "B1s"
+variable "vm_name" {
+  default = "B1s"
 }
 
-variable CIDR {
-    default = ["192.168.0.1/24"]
+variable "CIDR" {
+  default = ["192.168.0.1/24"]
 }
