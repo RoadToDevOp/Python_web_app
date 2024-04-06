6c39bfeb7f77e5608776a0c3f363a52fde8646fe
Eoghan O'Neill <eoghanoneill@live.co.uk>
2024-04-06 14:20:24 +0100

    Removal of old code

--- network.tf
+++ network.tf
@@ -9,42 +9,6 @@ resource "azurerm_network_security_group" "NSG" {
     name = "NSG"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    
-
-#     security_rule {
-#         name = "allow-http"
-#         priority = 100
-#         direction = "Inbound"
-#         access = "Allow"
-#         protocol = "Tcp"
-#         source_port_range = "*"
-#         destination_port_range = "80"
-#         source_address_prefix = "*"
-#         destination_address_prefix = "*"
-
-#     }
-#     security_rule {
-#         name                       = "allow-https"
-#         priority                   = 101
-#         direction                  = "Inbound"
-#         access                     = "Allow"
-#         protocol                   = "Tcp"
-#         source_port_range          = "*"
-#         destination_port_range     = "443"
-#         source_address_prefix      = "*"
-#         destination_address_prefix = "*"
-#   }
-#     security_rule {
-#         name                       = "allow-ssh"
-#         priority                   = 102
-#         direction                  = "Inbound"
-#         access                     = "Allow"
-#         protocol                   = "Tcp"
-#         source_port_range          = "*"
-#         destination_port_range     = "22"
-#         source_address_prefix      = "*"
-#         destination_address_prefix = "*"
-#   }
 }
 
 resource "azurerm_network_security_rule" "allowhttp" {
