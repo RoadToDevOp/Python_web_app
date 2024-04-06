622003795a5a9ffc71d98cc7b2ef87282921d5fe
Eoghan O'Neill <eoghanoneill@live.co.uk>
2024-04-06 13:32:30 +0100

    Upgraded to Standard Sku's

--- network.tf
+++ network.tf
@@ -64,7 +64,7 @@ resource "azurerm_public_ip" "pubip" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     allocation_method = "Static"
-    sku = "Basic"
+    sku = "Standard"
     domain_name_label = "eoghantestwebapp"
     sku_tier = "Regional"
 
@@ -74,7 +74,7 @@ resource "azurerm_lb" "Primary_lb" {
     name = "primary_lb"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    sku = "Basic"
+    sku = "Standard"
     frontend_ip_configuration {
       name = "Web_IP"
       public_ip_address_id = azurerm_public_ip.pubip.id
