560ced5e941393660615c4cf48c66d879db64176
Eoghan O'Neill <eoghanoneill@live.co.uk>
2024-04-08 18:53:13 +0100

    yes

--- Azure stuff/database.tf
+++ Azure stuff/database.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sqlstorage" {
-  name = "sql_storage"
+  name = "sqlstorage"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   account_tier = "Standard"
